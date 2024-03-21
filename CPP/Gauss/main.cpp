4924c70d02695201831d7ffb9ccc6edc13b59ee6
Bl4ckk3ys <ilchechnev@gmail.com>
2024-03-21 16:23:49 +0300

    123

---
+++ CPP/Gauss/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "gauss.h"
+
+
+int main()
+{
+  double** a, * y, * x;
+  int n;
+  std::cout << "Введите количество уравнений: ";
+  std::cin >> n;
+  a = new double* [n];
+  y = new double[n];
+  for (int i = 0; i < n; i++)
+  {
+    a[i] = new double[n];
+    for (int j = 0; j < n; j++)
+    {
+      std::cout << "a[" << i << "][" << j << "]= ";
+      std::cin >> a[i][j];
+    }
+  }
+  for (int i = 0; i < n; i++)
+  {
+    std::cout << "y[" << i << "]= ";
+    std::cin >> y[i];
+  }
+  Print(a, y, n);
+  x = gauss(a, y, n);
+  for (int i = 0; i < n; i++)
+    std::cout << "x[" << i << "]=" << x[i] << std::endl;
+  for (int i = 0; i < n; i++)
+    delete[] a[i];
+  delete[] a;
+  delete[] x;
+  delete[] y;
+
+  return 0;
+}
