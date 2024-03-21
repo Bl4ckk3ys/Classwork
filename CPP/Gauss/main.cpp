abc2e90c7df8af21c4ec97d0f82d1cee4800c337
Bl4ckk3ys <ilchechnev@gmail.com>
2024-03-21 16:39:39 +0300

    123

--- CPP/Gauss/main.cpp
+++ CPP/Gauss/main.cpp
@@ -4,35 +4,39 @@
 
 int main()
 {
-  double** a, * y, * x;
-  int n;
-  std::cout << "Введите количество уравнений: ";
-  std::cin >> n;
-  a = new double* [n];
-  y = new double[n];
-  for (int i = 0; i < n; i++)
-  {
+    double** a, * y, * x;
+    int n;
+    std::cout << "Введите количество уравнений: ";
+    std::cin >> n;
+    a = new double* [n];
+    y = new double[n];
+    for (int i = 0; i < n; i++){
     a[i] = new double[n];
-    for (int j = 0; j < n; j++)
-    {
+    for (int j = 0; j < n; j++){
       std::cout << "a[" << i << "][" << j << "]= ";
       std::cin >> a[i][j];
     }
   }
-  for (int i = 0; i < n; i++)
-  {
-    std::cout << "y[" << i << "]= ";
-    std::cin >> y[i];
-  }
-  Print(a, y, n);
-  x = gauss(a, y, n);
-  for (int i = 0; i < n; i++)
-    std::cout << "x[" << i << "]=" << x[i] << std::endl;
+    for (int i = 0; i < n; i++){
+        std::cout << "y[" << i << "]= ";
+        std::cin >> y[i];
+    }
+    Print(a, y, n);
+    x = gauss(a, y, n);
+    for (int i = 0; i < n; i++)
+        std::cout << "x[" << i << "]=" << x[i] << std::endl;
+
+    for (int i = 0; i < n; i++){
+        
+    }
+
+
+
   for (int i = 0; i < n; i++)
     delete[] a[i];
-  delete[] a;
-  delete[] x;
-  delete[] y;
+    delete[] a;
+    delete[] x;
+    delete[] y;
 
-  return 0;
+    return 0;
 }
