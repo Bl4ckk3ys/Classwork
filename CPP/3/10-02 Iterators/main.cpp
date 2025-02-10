3194ab7a65653985213f3a9c7d8ba7ea8f8e7a7a
Bl4ckk3ys <ilchechnev@gmail.com>
2025-02-10 12:04:09 +0300

    23

--- CPP/3/10-02 Iterators/main.cpp
+++ CPP/3/10-02 Iterators/main.cpp
@@ -2,7 +2,10 @@
 
 int main(){
     IntArr intArr;
+    for(Iterator i = intArr.begin(); i != intArr.end(); i++ ){
+        std::cin >> *i;
 
+    }
     for(auto it: intArr){
         std::cout<<it<<"\n";
     }
