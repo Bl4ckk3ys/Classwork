6039a4adff57722e67d28c797cb9f579e4793994
Bl4ckk3ys <ilchechnev@gmail.com>
2025-02-24 10:39:48 +0300

    sdf

--- CPP/3/24-02 Queue/main.cpp
+++ CPP/3/24-02 Queue/main.cpp
@@ -4,5 +4,11 @@ int main(){
     arr.push(10);
     arr.push(51);
     std::cout<< arr.pop() << "\n" << arr.pop() << "\n";
+    try{
+        std::cout<< arr.pop() << "\n";
+    }
+    catch(const char* error){
+        std::cout << error << "\n";
+    }
     return 0;
 }
